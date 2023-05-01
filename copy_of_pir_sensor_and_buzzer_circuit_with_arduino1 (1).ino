30fdf611d6044efb7e562153c2d5297615a6f165
santha995 <132038497+santha995@users.noreply.github.com>
2023-05-01 14:45:13 +0530

    Add files via upload

---
+++ copy_of_pir_sensor_and_buzzer_circuit_with_arduino1 (1).ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(3, INPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(3) == HIGH) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  delay(10); // Delay a little bit to improve simulation performance
+}
